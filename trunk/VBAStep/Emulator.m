26833c2c74d158b46c4ac9516afe6c37712dcf75
ribrdb <ribrdb@07d7a736-7aff-0310-a971-ad4f60e0f356>
2005-08-31 05:49:19 +0000

    Initial import of Cocoa/GNUStep frontend

---
+++ trunk/VBAStep/Emulator.m
@@ -0,0 +1,167 @@
+// VBAStep - Nintendo Gameboy Advance Debugger
+// Copyright (C) 2005 Rib Rdb
+
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2, or(at your option)
+// any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+#import "Emulator.h"
+#include <string.h>
+#include "GBA.h"
+
+#define COND_BLOCK_EMU 1
+#define COND_UNBLOCK_EMU 2
+#define COND_EMU_SHUTDOWN 3
+
+extern int emulating;
+
+void debuggerMain()
+{
+}
+
+void debuggerSignal(int a, int b)
+{
+}
+
+void debuggerOutput(char *a, u32 b)
+{
+}
+
+void debuggerBreakOnWrite(u32 address, u32 oldvalue, u32 value, 
+                          int size, int t)
+{
+}
+ 
+void (*dbgMain)() = debuggerMain;
+void (*dbgSignal)(int, int) = debuggerSignal;
+void (*dbgOutput)(char *, u32) = debuggerOutput;
+
+extern void remoteInit();
+extern void remoteCleanUp();
+extern void remoteStubMain();
+extern void remoteStubSignal(int,int);
+extern void remoteOutput(char *, u32);
+extern void remoteSetProtocol(int);
+extern void remoteSetPort(int);
+
+extern char cpuBreakLoop;
+
+@implementation Emulator
+
+- (id) init
+{
+  self = [super init];
+  gba = GBASystem;
+  shutdown = YES;
+  emulating = NO;
+  lock = [[NSConditionLock alloc] init];
+  return self;
+}
+
+- (void) dealloc
+{
+  [lock release];
+  [super dealloc];
+}
+
+- (NSImageRep*) getImageRep
+{
+  NSBitmapImageRep *rep = [NSBitmapImageRep alloc];
+  NSImage *image;
+  unsigned char *planes[2] = {((unsigned char*)pix) + 4*241, nil };
+  //memset(pix, 0xFFFFFFFF, 4*241*162);
+  rep = [rep initWithBitmapDataPlanes:planes
+             pixelsWide:240
+             pixelsHigh:160
+             bitsPerSample:8
+             samplesPerPixel:3
+             hasAlpha:NO
+             isPlanar:NO
+             colorSpaceName:NSDeviceRGBColorSpace
+             bytesPerRow:4*241
+             bitsPerPixel:32 ];
+  return [rep autorelease];
+}
+
+- (BOOL)loadRom:(NSString *)filename
+{
+  int size = CPULoadRom([filename cString]);
+  if ( size <= 0 ) {
+    return NO;
+  }
+  CPUInit("", NO);
+  CPUReset();
+
+  // XXX: load battery
+  if (dbgMain == remoteStubMain) {
+    remoteCleanUp();
+    [self setRemoteDebugger:NO];
+  }
+  debugger = NO;
+  return YES;
+}
+extern void remoteStubMain();
+
+- (void)run:(id)sender
+{
+  NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+  // Cocoa shouldn't be called from this thread so it
+  // should be fine to release this at the end
+  while (emulating) {
+    if (NO) { // XXX: paused
+      [lock lockWhenCondition: COND_UNBLOCK_EMU];
+      [lock unlock];
+    } else {
+      if (debugger)
+        remoteStubMain();
+      else
+        gba.emuMain(gba.emuCount);
+    }
+  }
+  [lock lock];
+  shutdown = YES;
+  [lock unlockWithCondition: COND_EMU_SHUTDOWN];
+  [pool release];
+}
+
+- (void)startRunning {
+  shutdown = NO;
+  emulating = YES;
+  [NSThread detachNewThreadSelector:@selector(run:)
+            toTarget:self
+            withObject:self];
+}
+
+- (void)shutDown {
+  if (shutdown)
+    return;
+  // XXX: Make sure nothing else can touch the condition!!
+  emulating = NO;
+  cpuBreakLoop = YES;
+  [lock lockWhenCondition: COND_EMU_SHUTDOWN];
+  [lock unlock];
+}
+
+- (void)setRemoteDebugger:(BOOL)hasDebugger {
+  if (hasDebugger) {
+    dbgMain = remoteStubMain;
+    dbgSignal = remoteStubSignal;
+    dbgOutput = remoteOutput;
+  } else {
+    dbgMain = debuggerMain;
+    dbgSignal = debuggerSignal;
+    dbgOutput = debuggerOutput;
+  }
+}
+
+@end
---
+++ trunk/VBAStep/MainController.m
@@ -0,0 +1,139 @@
+// VBAStep - Nintendo Gameboy Advance Debugger
+// Copyright (C) 2005 Rib Rdb
+
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2, or(at your option)
+// any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+#import "MainController.h"
+#import "Emulator.h"
+#import <Renaissance/Renaissance.h>
+
+#include "GBA.h"
+
+void initSystem();
+static MainController *controllerInstance = nil;
+static Emulator *emulator = nil;
+static NSImageRep *screenImage = nil;
+
+@implementation MainController
+
+- (void)applicationWillFinishLaunching: (NSNotification *)aNotification
+{
+  controllerInstance = self;
+  [NSBundle loadGSMarkupNamed: @"MainWindow"  owner: self];
+  initSystem();
+}
+
+- (void)loadRom:(NSString*)filename
+{
+  [emulator shutDown];
+  if (emulator == nil)
+    emulator = [[Emulator alloc] init];
+  [emulator loadRom:filename];
+  screenImage = [emulator getImageRep];
+  [display setImageRep: screenImage];
+}
+
+- (BOOL)application:(NSApplication *)theApp openFile:(NSString*)filename
+{
+  [self loadRom:filename];
+  [gbaWindow makeKeyAndOrderFront:self];
+  [emulator startRunning];
+  return YES;
+}
+
++ (void)refreshDisplays
+{
+  [controllerInstance performSelectorOnMainThread:@selector(updateDisplay:)
+                      withObject:nil
+                      waitUntilDone:NO];
+}
+
+- (void)updateDisplay:(id)sender {
+  [display setNeedsDisplay:YES];
+}
+
+- (void)open:(id)sender {
+  NSOpenPanel *panel = [NSOpenPanel openPanel];
+  if ([panel runModalForTypes:[NSArray arrayWithObjects:@"gba",nil]]
+      == NSOKButton) {
+    NSArray *filenames = [panel filenames];
+    NSString *rom = [filenames objectAtIndex:0];
+    [self application:nil
+          openFile:rom];
+  }
+}
+
++ (void)displaySpeed:(int)speed
+{
+  [controllerInstance->gbaWindow
+        setTitle:[NSString stringWithFormat:@"VBAStep (%d%%)",speed]];
+}
+
+
+-(void) toggleLayer:(id)sender
+{
+  unsigned int mask = 0x0100 << [sender tag];
+  if ([sender state]) {
+    layerSettings &= ~mask;
+  } else {
+    layerSettings |= mask;
+  }
+  layerEnable = DISPCNT & layerSettings;
+}
+
+- (void)windowWillClose:(NSNotification*)notification
+{
+  if ([notification object] == gbaWindow) {
+    [emulator shutDown];
+  }
+}
+
+- (void)showLayers:(id)sender
+{
+  [layersWindow orderFront:self];
+}
+
+- (void)connectToGdb:(id)sender
+{
+  NSOpenPanel *panel = [NSOpenPanel openPanel];
+  [gbaWindow orderOut:self];
+  [panel setAccessoryView:selectPortView];
+  if ([panel runModalForTypes:[NSArray arrayWithObjects:@"gba",nil]]
+      == NSOKButton) {
+    int port = [selectPortField intValue];
+    NSArray *filenames = [panel filenames];
+    NSString *rom = [filenames objectAtIndex:0];
+
+    [self loadRom:rom];
+
+    remoteSetProtocol(0);
+    remoteSetPort(port);
+    remoteInit();
+
+    debugger = YES;
+
+    [emulator setRemoteDebugger: YES];
+    [gbaWindow makeKeyAndOrderFront:self];
+    [emulator startRunning];
+  }
+}
+
+- (void) applicationWillTerminate:(NSNotification*)not
+{
+  [emulator shutDown];
+}
+
+@end
+

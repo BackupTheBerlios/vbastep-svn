2eb2681ac19e458fb3283c911bad0c81f306d2fd
ribrdb <ribrdb@07d7a736-7aff-0310-a971-ad4f60e0f356>
2005-09-06 07:03:15 +0000

    New class for managing updates to memory viewers

---
+++ trunk/VBAStep/MemLogger.m
@@ -0,0 +1,83 @@
+// VBAStep - Nintendo Gameboy Advance Debugger -*- objc -*-
+// Copyright (C) 2005 Rib Rdb
+
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2, or(at your option)
+// any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+#import "MemLogger.h"
+#include "System.h"
+
+static MemLogger *instance = nil;
+
+@implementation MemLogger
+- (id)init {
+  self = [super init];
+  instance = self;
+  palLog = [[PCList alloc] init];
+  return self;
+}
+
+- (void) dealloc {
+  [palLog release];
+  [super dealloc];
+}
+
+- (void)windowDidExpose:(NSNotification*)not
+{
+  if ([not object] == palWindow) {
+    trackPals = YES;
+  }
+}
+
+- (void)windowWillClose:(NSNotification*)not
+{
+  if ([not object] == palWindow) {
+    trackPals = NO;
+  }
+}
+
+- (void)updateWindows
+{
+  int i = [palLog size];
+  while (i-- > 0) {
+    int j;
+    u32 color = [palLog itemAtIndex:i]/2;
+    for (j = 0; j < 2; j++) {
+      if (color + j< 256) {
+        [bgPal setTileNeedsDisplay: color + j];
+      } else {
+        [spritePal setTileNeedsDisplay: ((color + j) & 255)];
+      }
+    }
+  }
+  [palLog swap];
+}
+
+@end
+
+void registerModified(u32 address) {
+
+}
+
+void paletteRamModified(u32 address) {
+  [instance->palLog addItem: address & 0xfffffffc];
+}
+
+void vramModified(u32 address) {
+  
+}
+
+void oamModified(u32 address) {
+
+}

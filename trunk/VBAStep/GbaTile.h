9621e558d7273bfcf0b11057dfba1119c6c16e64
ribrdb <ribrdb@07d7a736-7aff-0310-a971-ad4f60e0f356>
2005-12-21 06:05:23 +0000

    Add Tile Viewer

---
+++ trunk/VBAStep/GbaTile.h
@@ -0,0 +1,38 @@
+// VBAStep - Nintendo Gameboy Advance Debugger -*- objc -*-
+// Copyright (C) 2005 Rib Rdb
+
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2, or(at your option)
+// any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+#import <Foundation/Foundation.h>
+#import <AppKit/AppKit.h>
+#import "Tile.h"
+
+@interface GbaTile : NSObject <Tile>
+{
+  int pal_base;
+  NSBitmapImageRep *rep;
+  unsigned char *data;
+  BOOL eightbit;
+  NSColor *selection_color;
+  BOOL selected;
+}
+
+- (void)setSelectionColor:(NSColor*)aColor;
+- (void)setSelected:(BOOL)b;
+- (void)setPalBase:(int)b;
+- (int)palBase;
+- (void)setTileData:(unsigned char *)data;
+- (void)set8Bit:(BOOL)b;
+@end
